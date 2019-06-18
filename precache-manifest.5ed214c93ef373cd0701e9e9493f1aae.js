430ecbd0b5a6811c7c4b20f40fcb4df1692ec2f0
Marlon Gumban <gumban.marlon@yahoo.com>
2019-06-18 19:17:18 +0800

    Fix index.html

---
+++ precache-manifest.5ed214c93ef373cd0701e9e9493f1aae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0020615e07ce93ed8085435c1a5176d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8e60a690a8db0e511dc",
+    "url": "/static/js/2.7d13edf2.chunk.js"
+  },
+  {
+    "revision": "827d74bbd4d467c33467",
+    "url": "/static/js/main.bdc5db06.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0ee8c1d1863ad9353850f502f8d0c516",
+    "url": "/static/media/imgplaceholder.0ee8c1d1.png"
+  }
+]);
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Homepage from './components/pages/Homepage';
-// import Login from './components/pages/Loginpage';
-
-ReactDOM.render(<Homepage />, document.getElementById('root'));
